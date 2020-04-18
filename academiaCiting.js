b5019342fc079d26452e0cdae4ab926f5f7c5cef
Briar Carlile <bjcarlil@asu.edu>
2020-04-17 17:49:36 -0700

    Created extension icon and updated json and academiaCiting.js still no functionality. Running extension does not interact with page.

--- academiaCiting.js
+++ academiaCiting.js
@@ -1,19 +1,21 @@
-(function)() {
-  function captureSearchResults() {
+function captureSearchResults() {
+  var paperTitles = document.querySelectorAll('.gs_rt a');
+  Array.from(paperTitles).forEach(function(article) {
+    console.log(article.textContent) // Debugging to show results
+  }); // Grabs title info of element
 
-    var paperTitles = document.querySelectorAll('.gs_rt a');
-    Array.from(paperTitles).forEach(function(article) {
-      console.log(article.textContent) // Debugging to show results
-    }); // Grabs title info of element
+  var paperTitles = document.querySelectorAll('.gs_a');
+  console.log(paperTitles)
+  Array.from(paperTitles).forEach(function(article) {
+    console.log(article.textContent)
+  });// Grabs author information
 
-    var paperTitles = document.querySelectorAll('.gs_a');
-    Array.from(paperTitles).forEach(function(article) {
-      console.log(article.textContent)
-    });// Grabs author information
+  var paperTitles = document.querySelectorAll('.gs_rs')
+  Array.from(paperTitles).forEach(function(article) {
+    console.log(article.textContent)
+  }); // Grabs shortened summary
+  console.log("caprtureSearchResults function has run");
+}
 
-    var paperTitles = document.querySelectorAll('.gs_rs')
-    Array.from(paperTitles).forEach(function(article) {
-      console.log(article.textContent)
-    }); // Grabs shortened summary
-  }
-})();data_clk-atid
+console.log("academiaCiting.js has run");
+browser.browserAction.onClicked.addListener(captureSearchResults);
