8e48aa990940ae839666797b8af973d33d6c2dea
Briar Carlile <bjcarlil@asu.edu>
2020-09-18 15:20:17 -0700

    Preliminary push while integrating storage solutions

--- academiaCiting.js
+++ academiaCiting.js
@@ -1,21 +1,28 @@
+console.log("ASU Research Tool Deployed");
+
 function captureSearchResults() {
   var paperTitles = document.querySelectorAll('.gs_rt a');
   Array.from(paperTitles).forEach(function(article) {
+    try {
+      let titles = browser.storage.local.get({[paperTitles]: null});
+    browser.storage.local.set({[paperTitles]: titles[paperTitles]})
+    } catch (error) {
+      console.log(error);
+    }
+
     console.log(article.textContent) // Debugging to show results
   }); // Grabs title info of element
 
-  var paperTitles = document.querySelectorAll('.gs_a');
-  console.log(paperTitles)
-  Array.from(paperTitles).forEach(function(article) {
+  var paperAuthors = document.querySelectorAll('.gs_a');
+  console.log(paperAuthors)
+  Array.from(paperAuthors).forEach(function(article) {
     console.log(article.textContent)
   });// Grabs author information
 
-  var paperTitles = document.querySelectorAll('.gs_rs')
-  Array.from(paperTitles).forEach(function(article) {
+  var paperSummaries = document.querySelectorAll('.gs_rs')
+  Array.from(paperSummaries).forEach(function(article) {
     console.log(article.textContent)
   }); // Grabs shortened summary
   console.log("caprtureSearchResults function has run");
 }
-
-console.log("academiaCiting.js has run");
 browser.browserAction.onClicked.addListener(captureSearchResults);
