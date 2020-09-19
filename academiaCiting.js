8add78b4310237143e581b4f7093ee059b009c94
Briar Carlile <bjcarlil@asu.edu>
2020-09-19 10:16:12 -0700

    Successfull link between scripts

--- academiaCiting.js
+++ academiaCiting.js
@@ -1,28 +1,42 @@
-console.log("ASU Research Tool Deployed");
+// "use strict";
 
-function setItem() {
-  console.log("Items logged successfully");
-}
+// console.log("ASU Research Tool Deployed");
 
-function onError(error) {
-  console.log(error);
-}
-
-// let test = {
-//   name: "title",
-//   authors: "No Author",
-//   summary: "Here is the summary"
+// function setItem() {
+//   console.log("Items logged successfully");
 // }
 
-// browser.storage.local.set(test)
-//   .then(setItem, onError);
-//let settingItem = browser.storage.local.set(keys)
-function storeData(value)
+// function onError(error) {
+//   console.log(error);
+// }
+
+// browser.runtime.onMessage.addListener((message) => {
+//   if (message.command === "beastify") {
+//     insertBeast(message.beastURL);
+//   } else if (message.command === "reset") {
+//     removeExistingBeasts();
+//   }
+// });
+// browser.runtime.onMessage.addListener(request => {
+//   console.log("Message from the background script:");
+//   console.log(request.greeting);
+//   captureSearchResults();
+//   return Promise.resolve({response: "Hi from content script"});
+// });
+
+
+
+
+
+console.log("Content script is running");
+
+function storeData()
 {
   console.log("it's working");
-  console.log(value);
 }
 
+
+// The function to capture search results
 function captureSearchResults() {
   console.log("The capture function was successfully called.");
 
@@ -36,26 +50,35 @@ function captureSearchResults() {
     console.log(article.textContent)
   });
 
-  // var paperTitles = document.querySelectorAll('.gs_rt a');
-  // Array.from(paperTitles).forEach(storeData);
-  // console.log("it's NOT working");  
-  //   browser.storage.local.set({article})
-  //     .then(setItem, onError);
-  //   //console.log(article.textContent) // Debugging to show results
-  //}); // Grabs title info of element
+    // var paperTitles = document.querySelectorAll('.gs_rt a');
+    // Array.from(paperTitles).forEach(storeData);
+    // console.log("it's NOT working");  
+    //   browser.storage.local.set({article})
+    //     .then(setItem, onError);
+    //   //console.log(article.textContent) // Debugging to show results
+    //}); // Grabs title info of element
 
-  // var paperAuthors = document.querySelectorAll('.gs_a');
-  // console.log(paperAuthors)
-  // Array.from(paperAuthors).forEach(function(article) {
-  //   console.log(article.textContent)
-  // });// Grabs author information
+    // var paperAuthors = document.querySelectorAll('.gs_a');
+    // console.log(paperAuthors)
+    // Array.from(paperAuthors).forEach(function(article) {
+    //   console.log(article.textContent)
+    // });// Grabs author information
 
-  // var paperSummaries = document.querySelectorAll('.gs_rs')
-  // Array.from(paperSummaries).forEach(function(article) {
-  //   console.log(article.textContent)
-  // }); // Grabs shortened summary
-  // console.log("captureSearchResults function has run");
-}
+    // var paperSummaries = document.querySelectorAll('.gs_rs')
+    // Array.from(paperSummaries).forEach(function(article) {
+    //   console.log(article.textContent)
+    // }); // Grabs shortened summary
+    // console.log("captureSearchResults function has run");
+  }
 
+  // Checks for messages from background.
+  // browser.runtime.onMessage.addListener((message) => {
+  //   if (message.command === "cite") {
+  //     captureSearchResults();
+  //     console.log("Message received to start citing")
+  //   } else if (message.command === "reset") {
+  //     console.log("Received unknown message")
+  //   }
+  // });
 
-browser.browserAction.onClicked.addListener(captureSearchResults);
+browser.runtime.onMessage.addListener(captureSearchResults);
--- overwatch.js
+++ overwatch.js
@@ -1,1 +1,32 @@
+browser.browserAction.onClicked.addListener(alertCiting);
 
+
+// function alertCiting() {
+//     for (let tab of tabs) {
+//         browser.tabs.sendMessage(tabs[0].id, {
+//             command: "cite"
+//         });
+//     }
+// };
+
+function getActiveTab() {
+    return browser.tabs.query({active: true, currentWindow: true});
+}
+
+function alertCiting() {
+    console.log("alertCiting called");
+    const gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+    gettingActiveTab.then((tabs) => {
+      browser.tabs.sendMessage(tabs[0].id, "hello");
+      console.log("message sent");
+    });
+}
+
+// function alertCiting() {
+//     var sendAlert = browser.runtime.sendMessage({
+//         greeting: "Clear to start citing"
+//     });
+//     sendAlert.then(handleResponse, handleError);
+// }
+
+//window.addEventListener(handleResponse, handleError);
