fd504f3c2ac6a69f0d530d69c1dd3e05e9631f08
Briar Carlile <bjcarlil@asu.edu>
2020-11-06 12:45:52 -0700

    send_info and parsing of citations testing

--- academiaCiting.js
+++ academiaCiting.js
@@ -8,13 +8,21 @@ function storeData()
   
 }
 
+function parse(alpha) { // Used to extract the authors from the 'h3' header
+  var position = alpha.indexOf("-");
+  adjustedString = alpha.slice(0, position);
+  return adjustedString;
+}
 
 // The function to capture search results
 function captureSearchResults() {
   console.log("The capture function was successfully called.");
 
-  var docTitle = document.title;
-  console.log(docTitle);
+  //let temppaperUrls = document.querySelectorAll(".gs_rt a");
+  //let paperUrls = temppaperUrls.getAttribute('href');
+
+  //var docTitle = document.title;
+  //console.log(docTitle);
 
   var paperAuthors = document.querySelectorAll(".gs_a");
 
@@ -47,14 +55,46 @@ function captureSearchResults() {
     //let aggregateArray = new Array();
 
     for (var i = 0; i < authorArray; i++) {
+
+      //output = parse(paperAuthors[i].textContent);
+
       let newPaper = {
+        url: paperSummaries[i].getAttribute("href"),
         title: paperTitles[i].textContent,
         author: paperAuthors[i].textContent,
         summary: paperSummaries[i].textContent
       }
-      console.log(paperAuthors[i].textContent);
+      
+      //var link = paperSummaries.getAttribute("href");
+     
+      console.log(output);
+      //console.log(paperAuthors[i].textContent + ": " + paperTitles[i].textContent);
       aggregateArray[i] = newPaper;
     }
+}
+
+function send_info(alpha){
+  
+
+  let xhr = new XMLHttpRequest();
+  let url = "THIS NEEDS TO BE ADDED";
+
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  let json = JSON.stringify({
+    paperURL = alpha.url,
+    paperTitle = alpha.title,
+    paperAuthor = alpha.author,
+    paperSummary = alpha.summary
+  });
+
+  xhr.send(json);
+  // xhr.onreadystatechange = function () {
+  //   if (xhr.readyState === 4 && xhr.status === 200) {
+      
+  //   }
+  // }
 }
 
 browser.runtime.onMessage.addListener(captureSearchResults); // Checks for messages from the background script
