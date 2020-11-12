238f5002c3eef1f1dc3f0d028dd063646b7f80ea
Briar Carlile <bjcarlil@asu.edu>
2020-11-12 15:40:22 -0700

    Completed POST Request function & code cleaning

--- academiaCiting.js
+++ academiaCiting.js
@@ -1,43 +1,22 @@
 console.log("Content script is running");
 let aggregateArray = new Array();
 
-// Converts search array data to objects to be stored in local storage
-function storeData()
-{
-  console.log("storeData function was successfully called.");
-  
-}
-
-function parse(alpha) { // Used to extract the authors from the 'h3' header
-  var position = alpha.indexOf("-");
-  adjustedString = alpha.slice(0, position);
-  return adjustedString;
-}
-
 // The function to capture search results
 function captureSearchResults() {
   console.log("The capture function was successfully called.");
 
-  //let temppaperUrls = document.querySelectorAll(".gs_rt a");
-  //let paperUrls = temppaperUrls.getAttribute('href');
-
-  //var docTitle = document.title;
-  //console.log(docTitle);
-
   var paperAuthors = document.querySelectorAll(".gs_a");
+  var paperTitles = document.querySelectorAll(".gs_rt a");
+  var paperSummaries = document.querySelectorAll(".gs_rs");
+
 
   Array.from(paperAuthors).forEach(function(article) {
   }); // Can be used for debugging
-
-  var paperTitles = document.querySelectorAll(".gs_rt a");
-
   Array.from(paperTitles).forEach(function(article) {
   }); // Can be used for debugging
-
-  var paperSummaries = document.querySelectorAll(".gs_rs");
-
   Array.from(paperSummaries).forEach(function(article) {
   }); // Can be used for debugging
+
 
     console.log("Successful parsing of research results.")
 
@@ -52,49 +31,35 @@ function captureSearchResults() {
       console.log("There are an unequal number of authors and titles.");
     }
 
-    //let aggregateArray = new Array();
-
     for (var i = 0; i < authorArray; i++) {
-
-      //output = parse(paperAuthors[i].textContent);
-
       let newPaper = {
         url: paperSummaries[i].getAttribute("href"),
         title: paperTitles[i].textContent,
         author: paperAuthors[i].textContent,
-        summary: paperSummaries[i].textContent
+        summary: paperSummaries[i].textContent,
+        citing: null
       }
-      
-      //var link = paperSummaries.getAttribute("href");
-     
-      console.log(output);
-      //console.log(paperAuthors[i].textContent + ": " + paperTitles[i].textContent);
+      send_info(JSON.stringify(newPaper));
       aggregateArray[i] = newPaper;
     }
 }
 
 function send_info(alpha){
-  
-
   let xhr = new XMLHttpRequest();
-  let url = "THIS NEEDS TO BE ADDED";
-
+  let url = "localhost:6161";
+  console.log(alpha); // Debug stringify object
   xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Content_Type", "application/json");
 
-  let json = JSON.stringify({
-    paperURL = alpha.url,
-    paperTitle = alpha.title,
-    paperAuthor = alpha.author,
-    paperSummary = alpha.summary
-  });
-
-  xhr.send(json);
-  // xhr.onreadystatechange = function () {
-  //   if (xhr.readyState === 4 && xhr.status === 200) {
-      
-  //   }
-  // }
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+       console.log("POST Request Success");
+    }
+    else {
+      console.log("POST Request Failed")
+    }
+  }
+  xhr.send(alpha);
 }
 
 browser.runtime.onMessage.addListener(captureSearchResults); // Checks for messages from the background script
